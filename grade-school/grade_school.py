6a08182ca2804b2fbb786dc5f94376bc444722d0
karlhub <carles.bafaluy@gmail.com>
2020-09-11 21:32:50 +0200

    exercism 2020-09-11

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,12 +1,41 @@
 class School:
     def __init__(self):
-        pass
+        self.school = dict()
 
     def add_student(self, name, grade):
-        pass
+        lst_grade = self.school.get(grade,[])
+        lst_grade.append(name)
+        self.school[grade] = lst_grade
 
     def roster(self):
-        pass
+        list_grades = [gr for gr in self.school.keys()]
+        list_grades.sort()
+        list_roster = []
+        for gr in list_grades:
+            list_gr = sorted(self.school.get(gr,[]))
+            for st in list_gr:
+                list_roster.append(st)
+        return (list_roster)
 
     def grade(self, grade_number):
-        pass
+        return (sorted(self.school.get(grade_number,[])))
+
+# Program tester
+#sch = School()
+#while True:
+#    student = input ("Student: ")
+#    if student == "done":
+#        break
+#    grade = input ("Grade: ")
+#    sch.add_student(student, grade)
+#    print ("School: ", sch.school)
+
+#print ("Roster: ", sch.roster())
+
+#while True:
+#    grade = input ("Grade to search: ")
+#    if grade == "done":
+#        break
+#    print ("Students: ", sch.grade(grade))
+
+#print ("=> End of program")
