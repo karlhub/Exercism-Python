db24ea86f051b96b986e729db9d2362311bb5583
karlhub <carles.bafaluy@gmail.com>
2020-09-12 18:01:00 +0200

    exercism 2020-09-12

--- grade-school/grade_school.py
+++ grade-school/grade_school.py
@@ -1,24 +1,28 @@
 class School:
     def __init__(self):
-        self.school = dict()
+#        self.school = dict()
+        self.m_roster = {}
 
     def add_student(self, name, grade):
-        lst_grade = self.school.get(grade,[])
-        lst_grade.append(name)
-        self.school[grade] = lst_grade
+#        lst_grade = self.school.get(grade,[])
+#        lst_grade.append(name)
+#        self.school[grade] = lst_grade
+        self.m_roster[name] = grade
 
     def roster(self):
-        list_grades = [gr for gr in self.school.keys()]
-        list_grades.sort()
-        list_roster = []
-        for gr in list_grades:
-            list_gr = sorted(self.school.get(gr,[]))
-            for st in list_gr:
-                list_roster.append(st)
-        return (list_roster)
+#        list_grades = [gr for gr in self.school.keys()]
+#        list_grades.sort()
+#        list_roster = []
+#        for gr in list_grades:
+#            list_gr = sorted(self.school.get(gr,[]))
+#            for st in list_gr:
+#                list_roster.append(st)
+#        return (list_roster)
+        return [v[0] for v in sorted(self.m_roster.items(), key = lambda x: (x[1], x[0]))]
 
     def grade(self, grade_number):
-        return (sorted(self.school.get(grade_number,[])))
+#        return (sorted(self.school.get(grade_number,[])))
+        return [v[0] for v in sorted(filter(lambda x: x[1] == grade_number, self.m_roster.items()))]
 
 # Program tester
 #sch = School()
