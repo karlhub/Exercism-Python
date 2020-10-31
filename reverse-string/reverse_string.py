f2ab0ff8f76cab15da77bb3176c22c8ca0f824da
karlhub <carles.bafaluy@gmail.com>
2020-10-31 19:49:23 +0100

    Exercism 2020-10-31

---
+++ reverse-string/reverse_string.py
@@ -0,0 +1,11 @@
+def reverse(text):
+    return text[::-1]
+
+# Program tester
+#while True:
+#    txt = input("Text: ")
+#    if txt == "done":
+#         break
+#    print(reverse(txt))
+
+#print("=> End of program")
